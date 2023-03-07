d89417104df82c9e3003bf641db9589b8f252d63
hamzabalazs <hamzabalazs99@gmail.com>
2023-03-07 14:10:22 +0200

    Refactored categories to use graphql

--- mozi-frontend/src/api/category/useCategories.ts
+++ mozi-frontend/src/api/category/useCategories.ts
@@ -1,11 +1,7 @@
 import { useEffect, useState } from "react";
-import {
-  AddCategoryAPI,
-  DeleteCategoryAPI,
-  getCategoryList,
-  UpdateCategoryAPI,
-} from "./CategoryApi";
 import { Category } from "../types";
+import { useQuery, gql, useMutation } from '@apollo/client'
+import { client } from "../../index";
 
 export type CategoryData = {
   categories: Category[];
@@ -16,43 +12,88 @@ export type CategoryData = {
   refetchData: () => Promise<void>;
 };
 
-export function useCategories(token?: string): CategoryData {
-  const [loading, setLoading] = useState(false);
-  const [categories, setCategories] = useState<Category[]>([]);
+const ADD_CATEGORY = gql`
+  mutation CreateCategory($input: AddCategoryInput!) {
+  createCategory(input: $input) {
+    id
+    name
+  }
+}
+`
 
+const UPDATE_CATEGORY = gql`
+  mutation UpdateCategory($input: UpdateCategoryInput!) {
+  updateCategory(input: $input) {
+    id
+    name
+  }
+}
+`
+
+const DELETE_CATEGORY = gql`
+  mutation DeleteCategory($input: DeleteCategoryInput!) {
+  deleteCategory(input: $input) {
+    id
+    name
+  }
+}
+`
+
+const GET_CATEGORIES = gql`
+  query GetCategories {
+  getCategories {
+    id
+    name
+  }
+}
+`
+
+export function useCategories(token?: string): CategoryData {
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [AddCategoryAPI] = useMutation(ADD_CATEGORY)
+  const [UpdateCategoryAPI] = useMutation(UPDATE_CATEGORY)
+  const [DeleteCategoryAPI] = useMutation(DELETE_CATEGORY)
+  const {data:categoriesData,loading} = useQuery(GET_CATEGORIES)
   async function refetchData() {
-    setLoading(true);
-    const categories = await getCategoryList();
-    setCategories(categories);
-    setLoading(false);
+    await client.refetchQueries({
+      include: [GET_CATEGORIES]
+    })
+    setCategories(categoriesData.getCategories)
   }
 
   async function addCategory(category: Omit<Category, "id">): Promise<boolean> {
     if (!token) return false;
-    const result = await AddCategoryAPI(category, token);
+    const result = await AddCategoryAPI({variables:{input:{name: category.name}}});
     refetchData();
-    return result;
+    if(result === undefined) return false;
+    return true;
   }
 
   // Update
   async function editCategory(category: Category): Promise<boolean> {
     if (!token) return false;
-    const result = await UpdateCategoryAPI(category, token);
+    const result = await UpdateCategoryAPI({variables:{input:{id:category.id,name: category.name}}});
     refetchData();
-    return result;
+    if(result === undefined) return false;
+    return true;
   }
 
   // Delete
   async function deleteCategory(id: string): Promise<boolean> {
     if (!token) return false;
-    const result = await DeleteCategoryAPI(id, token);
+    console.log(id)
+    const result = await DeleteCategoryAPI({variables:{input:{id}}});
     refetchData();
-    return result;
+    if(result === undefined) return false;
+    return true;
   }
 
   useEffect(() => {
-    refetchData();
-  }, []);
+    if(categoriesData){
+      refetchData();
+    }
+    
+  }, [categoriesData]);
 
   return {
     categories,
