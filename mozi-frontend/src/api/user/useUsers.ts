be20213380b553c5e2688dc075242039ddc4222d
hamzabalazs <hamzabalazs99@gmail.com>
2023-03-07 15:09:03 +0200

    Refactored users to work with new backend

--- mozi-frontend/src/api/user/useUsers.ts
+++ mozi-frontend/src/api/user/useUsers.ts
@@ -1,12 +1,7 @@
 import { useEffect, useState } from "react";
 import { User } from "../types";
-import {
-  DeleteUserAPI,
-  getUserList,
-  PostUserAPI,
-  UpdateCurrentUserAPI,
-  UpdateUserAPI,
-} from "./UserApi";
+import { gql, useQuery, useMutation } from "@apollo/client";
+import { client } from "../../index";
 
 export type UserData = {
   users: User[];
@@ -15,62 +10,130 @@ export type UserData = {
   editUser: (
     user: Omit<User, "role"> & { role?: User["role"] }
   ) => Promise<boolean>;
-  editCurrentUser: (user: User) => Promise<boolean>;
   deleteUser: (id: string) => Promise<boolean>;
   refetchData: () => Promise<void>;
 };
 
+const GET_USERS = gql`
+  query GetUsers {
+    getUsers {
+      id
+      first_name
+      last_name
+      email
+      role
+    }
+  }
+`;
+
+const ADD_USER = gql`
+  mutation CreateUser($input: AddUserInput!) {
+    createUser(input: $input) {
+      id
+      first_name
+      last_name
+      role
+      email
+    }
+  }
+`;
+
+const UPDATE_USER = gql`
+  mutation UpdateUser($input: UpdateUserInput!) {
+    updateUser(input: $input) {
+      id
+      first_name
+      last_name
+      role
+      email
+    }
+  }
+`;
+
+const DELETE_USER = gql`
+  mutation DeleteUser($input: DeleteUserInput!) {
+    deleteUser(input: $input) {
+      id
+      first_name
+      last_name
+      role
+      email
+    }
+  }
+`;
+
 export function useUsers(token?: string): UserData {
-  const [loading, setLoading] = useState(false);
   const [users, setUsers] = useState<User[]>([]);
+  const [PostUserAPI] = useMutation(ADD_USER);
+  const [UpdateUserAPI] = useMutation(UPDATE_USER);
+  const [DeleteUserAPI] = useMutation(DELETE_USER);
+  const { data: usersData, loading } = useQuery(GET_USERS);
 
   async function refetchData() {
-    setLoading(true);
-    const users = await getUserList();
-    setUsers(users);
-    setLoading(false);
+    await client.refetchQueries({
+      include: [GET_USERS],
+    });
+    setUsers(usersData.getUsers);
   }
 
   async function registerUser(
     user: Omit<User, "id" | "role">
   ): Promise<boolean> {
-    const result = await PostUserAPI(user);
+    const result = await PostUserAPI({
+      variables: {
+        input: {
+          first_name: user.first_name,
+          last_name: user.last_name,
+          email: user.email,
+          password: user.password,
+        },
+      },
+    });
     refetchData();
-    return result;
+    if (result === undefined) return false;
+    return true;
   }
 
-  async function editUser(user: Omit<User, "role"> & { role?: User["role"] }): Promise<boolean> {
+  async function editUser(
+    user: Omit<User, "role"> & { role?: User["role"] }
+  ): Promise<boolean> {
     if (!token) return false;
-    console.log(user);
-    const result = await UpdateUserAPI(user, token);
+    const result = await UpdateUserAPI({
+      variables: {
+        input: {
+          id: user.id,
+          first_name: user.first_name,
+          last_name: user.last_name,
+          email:user.email,
+          password:user.password,
+          role:user.role
+        },
+      },
+    });
     refetchData();
-    return result;
-  }
-
-  async function editCurrentUser(user: User): Promise<boolean> {
-    if (!token) return false;
-    const result = await UpdateCurrentUserAPI(user, token);
-    refetchData();
-    return result;
+    if (result === undefined) return false;
+    return true;
   }
 
   async function deleteUser(id: string): Promise<boolean> {
     if (!token) return false;
-    const result = await DeleteUserAPI(id, token);
+    const result = await DeleteUserAPI({variables:{input:{id}}});
     refetchData();
-    return result;
+    if (result === undefined) return false;
+    return true;
   }
 
   useEffect(() => {
-    refetchData();
-  }, []);
+    if(usersData){
+      refetchData();
+    }
+  }, [usersData]);
 
   return {
     users,
     usersLoading: loading,
     registerUser,
     editUser,
-    editCurrentUser,
     deleteUser,
     refetchData,
   };
